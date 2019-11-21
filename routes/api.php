f92fe9eaf91d350348a237b59a0da6ce2ef74f70
Agung <awagung9@gmail.com>
2019-11-21 09:55:18 +0700

    edit routing

--- routes/api.php
+++ routes/api.php
@@ -31,89 +31,52 @@ Route::group(['middleware' => 'auth:api'], function(){
 
     //ProjectController
     Route::resource('project', 'API\ProjectController');
-    // Route::post('create-project', 'API\ProjectController@store');
-    // Route::get('show-project/{project}', 'API\ProjectController@show');
-    Route::post('add-member-project', 'API\ProjectController@addMember');
-    // Route::put('update-project/{project}', 'API\ProjectController@update');
-    // Route::put('delete-project/{project}', 'API\ProjectController@destroy');
-    Route::post('get-member-project', 'API\ProjectController@getMember');
+    Route::post('project/member/{user}', 'API\ProjectController@addMember');
+    Route::get('project/member', 'API\ProjectController@getMember');
+    Route::delete('project/member/{user}', 'API\ProjectController@deleteMember');
     Route::get('myProject', 'API\ProjectController@myProject');
     Route::post('boardOfProject', 'API\ProjectController@boardOfProject');
-    Route::post('delete-member-project', 'API\ProjectController@deleteMember');
 
     //TaskController
     Route::resource('task', 'API\TaskController');
-    // Route::post('index-task', 'API\TaskController@index');
-    // Route::get('show-task/{task}', 'API\TaskController@show');
-    // Route::post('create-task', 'API\TaskController@store');
-    Route::post('add-member-task', 'API\TaskController@addMember');
-    // Route::put('update-task/{task}', 'API\TaskController@update');
-    // Route::put('delete-task/{task}', 'API\TaskController@destroy');
+    Route::post('task/member/{user}', 'API\TaskController@addMember');
+    Route::get('task/member', 'API\TaskController@getMember');
+    Route::delete('task/member/{user}', 'API\TaskController@deleteMember');
     Route::get('roadmap', 'API\TaskController@myTask');
     Route::get('reminder', 'API\TaskController@myUrgentTask');
-    Route::post('delete-member-task', 'API\TaskController@deleteMember');
-    Route::post('get-member-task', 'API\TaskController@getMember');
     Route::get('myTask', 'API\TaskController@myTask'); //Roadmap
     Route::get('myUrgentTask', 'API\TaskController@myUrgentTask'); //Reminder
 
     //BoardController
     Route::resource('board', 'API\BoardController');
-    // Route::post('index-board', 'API\BoardController@index');
-    // Route::post('create-board', 'API\BoardController@store');
-    // Route::get('show-board/{board}', 'API\BoardController@show');
-    Route::post('add-member-board', 'API\BoardController@addMember');
-    // Route::put('update-board/{board}', 'API\BoardController@update');
-    // Route::put('delete-board/{board}', 'API\BoardController@destroy');
+    Route::post('board/member/{user}', 'API\BoardController@addMember');
+    Route::get('board/member', 'API\BoardController@getMember');
+    Route::delete('board/member/{user}', 'API\BoardController@deleteMember');
     Route::get('myBoard', 'API\BoardController@myBoard');
     Route::post('board-of-project', 'API\BoardController@boardOfProject');
-    Route::post('delete-member-board', 'API\BoardController@deleteMember');
 
     //CardsController
     Route::resource('card', 'API\CardsController');
-    // Route::post('index-card', 'API\CardsController@index');
-    // Route::post('create-card', 'API\CardsController@store');
-    // Route::get('show-card/{cards}', 'API\CardsController@show');
-    Route::post('add-member-card', 'API\CardsController@addMember');
-    // Route::put('update-card/{cards}', 'API\CardsController@update');
-    // Route::put('delete-card/{cards}', 'API\CardsController@destroy');
+    Route::post('card/member/{user}', 'API\CardsController@addMember');
+    Route::get('card/member', 'API\CardsController@getMember');
+    Route::delete('card/member/{user}', 'API\CardsController@deleteMember');
     Route::get('myCard', 'API\CardsController@myCard');
 
     //GroupingController
     Route::resource('grouping', 'API\GroupingController');
-    // Route::post('create-grouping', 'API\GroupingController@store');
     Route::post('add-grouping', 'API\GroupingController@addTask');
     Route::post('list-groupinglist-grouping', 'API\GroupingController@myTask');
 
     //CheacklistController
     Route::resource('checklist', 'API\CheckListController');
-    // Route::post('index-checklist', 'API\CheckListController@index');
-    // Route::post('create-checklist', 'API\CheckListController@store');
-    // Route::get('show-checklist/{checklist}', 'API\CheckListController@show');
-    // Route::put('update-checklist/{checklist}', 'API\CheckListController@update');
-    // Route::put('delete-checklist/{checklist}', 'API\CheckListController@destroy');
 
     //CommentController
     Route::resource('comment', 'API\CommentController');
-    // Route::post('index-comment', 'API\CommentController@index');
-    // Route::post('create-comment', 'API\CommentController@store');
-    // Route::get('show-comment/{comment}', 'API\CommentController@show');
-    // Route::put('update-comment/{comment}', 'API\CommentController@update');
-    // Route::put('delete-comment/{comment}', 'API\CommentController@destroy');
 
     //LabelController
     Route::resource('label', 'API\LabelController');
-    // Route::post('index-label', 'API\LabelController@index');
-    // Route::post('create-label', 'API\LabelController@store');
-    // Route::get('show-label/{label}', 'API\LabelController@show');
-    // Route::put('update-label/{label}', 'API\LabelController@update');
-    // Route::put('delete-label/{label}', 'API\LabelController@destroy');
 
     //LinkController
     Route::resource('link', 'API\LinkController');
-    // Route::post('index-Link', 'API\LinkController@index');
-    // Route::post('create-Link', 'API\LinkController@store');
-    // Route::get('show-Link/{Link}', 'API\LinkController@show');
-    // Route::put('update-Link/{Link}', 'API\LinkController@update');
-    // Route::put('delete-Link/{Link}', 'API\LinkController@destroy');
 
 });
